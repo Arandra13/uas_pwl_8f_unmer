42c8706eedd256adcaf42cbd1ad7c9b27847febe
Arandra13 <91449316+Arandra13@users.noreply.github.com>
2022-07-08 21:43:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <html>
 
-<<<<<<< HEAD
 <p> 18083000063 ANDIKA MOHAMMAD ARANDRA PUTRA <P>
-<p> index baru 1
-
-=======
-<p> 18083000136 ADAM VIRGIANSA 8F <p>
-<p> index baru 1
->>>>>>> 217d35878c46cfc7330a4e447a6b2f71c4423ff7
+<p> index baru 1 <p>
 
 </html>
